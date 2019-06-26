1c413cfed274c94ddad22668ca6ff7cfa76ac0a0
mc-feng <734568955@qq.com>
2019-06-26 14:37:37 +0800

    RB预约初始化

---
+++ pages/index/index.js
@@ -0,0 +1,40 @@
+//index.js
+//获取应用实例
+const app = getApp()
+import http from '../../api/api'
+Page({
+  data: {
+  },
+  onLoad: function () {
+    // var that = this;
+    // that.checkOpenId();
+  },
+  onShow: function () { 
+  },
+  //防止网络延迟获取不到openid
+  // checkOpenId() {
+  //   setTimeout(res, 1000)
+  //   function res() {
+  //     if (app.globalData.openId) {
+
+  //     } else {
+  //       wx.login({
+  //         success: res => {
+  //           http.info({
+  //             data: {
+  //               code: res.code
+  //             },
+  //             success(res) {
+  //               app.globalData.openId = res.data.openid
+  //             },
+  //             fail(err) {
+  //               console.log(err)
+  //             }
+  //           })
+  //           // 发送 res.code 到后台换取 openId, sessionKey, unionId
+  //         }
+  //       })
+  //     }
+  //   }
+  // },
+})
---
+++ pages/personal/index/index.js
@@ -0,0 +1,30 @@
+// pages/personal/index/index.js
+const app = getApp()
+import http from '../../../api/api'
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    show:false,
+    result:[{},{}]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  },
+})
