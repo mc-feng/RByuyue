ecf73ba3a8771ed7094427554dc4bda7a442b459
xiashuang <hsiacool@163.com>
2019-06-27 18:01:29 +0800

    首页

---
+++ pages/order/index.js
@@ -0,0 +1,83 @@
+// pages/order/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    date:{}
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getDate()
+  },
+  getDate(){
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = now.getMonth() + 1;
+    var day = now.getDate();
+    if (month < 10) {
+      month = "0" + month;
+    }
+    if (day < 10) {
+      day = "0" + day;
+    }
+    var date = year+'-'+month+'-'+day;
+    var timestamp = Date.parse(date);
+    console.log(timestamp)
+    // for(var i=0;i<7;i++){
+    //   date.push(timestamp+)
+    // }
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
